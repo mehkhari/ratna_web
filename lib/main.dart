7f656c3bde9d2a69c8baa61bf0e4e0cad157bfb0
saikumargedela <saikumarg@fininfocom.com>
2022-01-06 16:38:34 +0530

    changed app name

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Video Player',
+      title: 'Dance Practice',
       debugShowCheckedModeBanner: false,
       home: MyHomePage(),
     );
